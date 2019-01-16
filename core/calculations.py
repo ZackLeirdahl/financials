bed25621d819717791dbe31a4f4bacc7961f150f
Zack Leirdahl <zackleirdahl@Zacks-MacBook-Pro.local>
2019-01-15 18:11:57 -0600

    added new calc, updated databridge.py

--- core/calculations.py
+++ core/calculations.py
@@ -1,16 +1,31 @@
 from stock import Stock
 from iex import StockReader
+from robinhood import Robinhood
+from option import Option
 import pandas as pd
 
 
 #VWAP 1 Day = (Sum of number of shares bought * price) / number of shares bought
-s = StockReader(['NVDA'])
-chart = s.get_chart(range='1d')
-vw = 0
-volume = 0
-for point in chart:
-    avg = float(point['marketAverage'])
-    vol = float(point['marketVolume'])
-    vw += avg * vol
-    volume += vol
-vwap = vw/volume
+# s = StockReader(['NVDA'])
+# chart = s.get_chart(range='1d')
+# vw = 0
+# volume = 0
+# for point in chart:
+#     avg = float(point['marketAverage'])
+#     vol = float(point['marketVolume'])
+#     vw += avg * vol
+#     volume += vol
+# vwap = vw/volume
+#######################
+
+#Get the option with the highest volume
+r = Robinhood()
+vol = 0
+highest_vol = None
+options = r.get_options('AMD', ['20190118','20190125','20190201','20190208','20190215'],'call')
+for option in options:
+    o = Option(option['url'])
+    if o.volume > vol:
+        vol = o.volume
+        highest_vol = o
+##########
--- core/databridge.py
+++ core/databridge.py
@@ -1,7 +1,7 @@
 from firebase import Firebase
 from stock import Stock
 from option import Option
-from positions import OptionPositions
+from positions import Position, Positions
 
 class DataBridge(Firebase):
     def __init__(self):
@@ -17,3 +17,12 @@ class DataBridge(Firebase):
     def update_stock(self, ticker_symbol):
         stock = Stock(ticker_symbol)
         self.update_document('stocks',ticker_symbol, stock.data)
+
+    def update_positions(self):
+        p = Positions()
+        positions = p.positions
+        for position in positions:
+            self.add_document('positions',position.id,position.data)
+
+db = DataBridge()
+db.update_positions()    
