57aaee777bf80adc499cf8ddc3551f07ef2b4cde
Zack Leirdahl <zackleirdahl@Zacks-MacBook-Pro.local>
2019-01-16 17:43:02 -0600

    got rid of merge markers

--- core/calculations.py
+++ core/calculations.py
@@ -1,21 +1,10 @@
 from stock import Stock
 from iex import StockReader
-<<<<<<< HEAD
 from robinhood import Robinhood
 from option import Option
-=======
-from option import Option
-from robinhood import Robinhood
->>>>>>> a3fc165c3005ed352acdea0102006b50a2d44c48
-import pandas as pd
-
 
 #VWAP 1 Day = (Sum of number of shares bought * price) / number of shares bought
-<<<<<<< HEAD
-# s = StockReader(['NVDA'])
-=======
 # s = StockReader(['NFLX'])
->>>>>>> a3fc165c3005ed352acdea0102006b50a2d44c48
 # chart = s.get_chart(range='1d')
 # vw = 0
 # volume = 0
@@ -25,23 +14,7 @@ import pandas as pd
 #     vw += avg * vol
 #     volume += vol
 # vwap = vw/volume
-<<<<<<< HEAD
 #######################
-
-#Get the option with the highest volume
-r = Robinhood()
-vol = 0
-highest_vol = None
-options = r.get_options('AMD', ['20190118','20190125','20190201','20190208','20190215'],'call')
-for option in options:
-    o = Option(option['url'])
-    if o.volume > vol:
-        vol = o.volume
-        highest_vol = o
-##########
-=======
-# print(vwap)
-###########################
 
 #Find highest_volume for option chain
 r = Robinhood()
