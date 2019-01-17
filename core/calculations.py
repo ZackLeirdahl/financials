02a2149dd3309fd41580b145047b2a4667b1c649
ZackLeirdahl <zackleirdahl@gmail.com>
2019-01-17 16:36:18 -0600

    updated calcs

--- core/calculations.py
+++ core/calculations.py
@@ -2,30 +2,21 @@ from stock import Stock
 from iex import StockReader
 from option import Option
 from robinhood import Robinhood
-import pandas as pd
 
+def get_vwap_daily(ticker_symbol, vw = 0, volume = 0):
+    s = StockReader([ticker_symbol])
+    for point in s.get_chart(range='1d'):
+        avg = float(point['marketAverage'])
+        vol = float(point['marketVolume'])
+        vw += avg * vol
+        volume += vol
+    return round(vw/volume,2)
 
-#VWAP 1 Day = (Sum of number of shares bought * price) / number of shares bought
-# s = StockReader(['NFLX'])
-# chart = s.get_chart(range='1d')
-# vw = 0
-# volume = 0
-# for point in chart:
-#     avg = float(point['marketAverage'])
-#     vol = float(point['marketVolume'])
-#     vw += avg * vol
-#     volume += vol
-# vwap = vw/volume
-# print(vwap)
-###########################
-
-#Find highest_volume for option chain
-r = Robinhood()
-volume = 0
-highest_volume = None
-for option in r.get_options('NFLX',['2019-01-18','2019-01-25'],'call'):
-    temp_vol = r.get_option_market_data(option['id'])['volume']
-    if temp_vol > volume:
-        volume = temp_vol
-        highest_volume = option
-###################################
+def get_highest_volume(ticker_symbol, dates, type, volume = 0, highest_volume = None):
+    r = Robinhood()
+    for option in r.get_options(ticker_symbol, dates, type):
+        temp_vol = r.get_option_market_data(option['id'])['volume']
+        if temp_vol > volume:
+            volume = temp_vol
+            highest_volume = option
+    return highest_volume
