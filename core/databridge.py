17d74e17e8fe3e0069877549b2dcb3b203b5a47e
Zack Leirdahl <zackleirdahl@Zacks-MacBook-Pro.local>
2019-01-18 17:07:11 -0600

    fixed databridge

--- core/databridge.py
+++ core/databridge.py
@@ -1,11 +1,6 @@
 from firebase import Firebase
 from stock import Stock
-from option import Option
-<<<<<<< HEAD
-from positions import Position, Positions
-=======
 from positions import Positions
->>>>>>> eeb668488f0764af353528f4ad60e94b2b0fab42
 
 class DataBridge(Firebase):
     def __init__(self):
@@ -20,18 +15,6 @@ class DataBridge(Firebase):
 
     def update_stock(self, ticker_symbol, collection = 'stocks'):
         stock = Stock(ticker_symbol)
-<<<<<<< HEAD
-        self.update_document('stocks',ticker_symbol, stock.data)
-
-    def update_positions(self):
-        p = Positions()
-        positions = p.positions
-        for position in positions:
-            self.add_document('positions',position.id,position.data)
-
-db = DataBridge()
-db.update_positions()    
-=======
         self.update_document(collection, ticker_symbol, stock.data)
 
     def update_positions(self, collection = 'positions'):
@@ -41,4 +24,3 @@ db.update_positions()
         old_positions = list(set(previous_positions) - set(new_positions))
         for pos in old_positions: self.remove_document(collection, pos)
         for k, v in new_positions.items(): self.add_document(collection, k, v)
->>>>>>> eeb668488f0764af353528f4ad60e94b2b0fab42
