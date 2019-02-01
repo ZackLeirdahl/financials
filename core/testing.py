13e8b191bf50f073a39da4179aa3f1b32597dbce
ZackLeirdahl <zackleirdahl@gmail.com>
2019-02-01 16:53:39 -0600

    new test

--- core/testing.py
+++ core/testing.py
@@ -1,5 +1,6 @@
 from robinhood import Robinhood
-
-r = Robinhood()
-print(r.get_highest_volume_strike('AMD',1,'call'))
-#print(r.get_option_market_data('7eac4691-074d-4fc7-8a7f-3812e198977c'))
+from iex import StockReader
+stocks = ['BAC','AMD','MSFT','FB','PYPL','SONO','GOOS']
+for s in stocks:
+    sr = StockReader([s])
+    print(s + '\n' + str(sr.get_vwap_daily()))
